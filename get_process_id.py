ba1737f348009c03cc1ed263580dc27def61fe44
Ilya Emelyanov <emel.9616@gmail.com>
2021-01-28 11:27:11 +0700

    Update get_process_id.py

--- get_process_id.py
+++ get_process_id.py
@@ -3,7 +3,7 @@
 import json
 
 def get_process_id():
-    with open('./ihub_parameters.json', "rb") as PFile:
+    with open("ihub_parameters.json", "rb") as PFile:
         ihub_data = json.loads(PFile.read().decode('utf-8'))
 
     return ihub_data['processId']
