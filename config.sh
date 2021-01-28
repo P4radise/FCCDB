bc9efd59cc8edbc1de0ac6c85e26e72c52860472
Ilya Emelyanov <emel.9616@gmail.com>
2021-01-28 13:48:02 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -45,10 +45,9 @@ function get_import_id(){
 function get_value_by_attrib_name(){
     local PY_SCRIPT="$1"
     local IHUB_PARAMETERS="$2"
-    local ATTRIBUTE_NAME="$3"
 
     PARAMETERS=$(cat $SCRIPT_DIR"/$IHUB_PARAMETERS")
-    RES=$("$PY_SCRIPT" "$PARAMETERS" "$ATTRIBUTE_NAME")
+    RES=$("$PY_SCRIPT" "$PARAMETERS")
     echo $RES
 }
 
@@ -64,7 +63,7 @@ export IMPORT_CURL_RETRIES="20"
 export WAIT_SEC_BEFORE_NEXT_RETRY="300"
 export DAT_CELLS_PER_FILE="5000000"
 export LOG_URL=$(get_property $SCRIPT_DIR"/settings" "URL")
-export IHUB_PROCESS=$(get_value_by_attrib_name $SCRIPT_DIR"/get_process_id.py" "ihub_parameters.json" "processId")
+export IHUB_PROCESS=$(get_value_by_attrib_name $SCRIPT_DIR"/get_value_by_attrib_name.py" "ihub_parameters.json")
 
 export API_UN=$(get_property $SCRIPT_DIR"/settings" "USERNAME")
 export API_PW=$(get_property $SCRIPT_DIR"/settings" "PWD")
